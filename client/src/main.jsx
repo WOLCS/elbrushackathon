d413ae85dff39ae0f30de127ff192dcb9435d2d7
Waldemar Kunz <w_kunz@web.de>
2022-12-17 14:37:12 +0100

    Add redux store and router provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider as StoreProvider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
+import { store } from './store/store'
 import { App } from './App'
+
+// console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-         <App />
-      </MantineProvider>
+      <StoreProvider store={store}>
+         <BrowserRouter>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+               <App />
+            </MantineProvider>
+         </BrowserRouter>
+      </StoreProvider>
    </React.StrictMode>
 )
---
+++ client/src/store/initialState.js
@@ -0,0 +1,61 @@
+export const initialState = {
+   scheme1: {
+      schemeId: 'scheme1',
+      title: 'Переговоры',
+      schemeNodes: {
+         sa1: {
+            schemeNodeId: 'sa1',
+            schemeId: 'scheme1',
+            elements: [
+               { elementId: 'esa11', elementType: 'text', elementText: 'Вам звонит HR и сообщает' },
+               { elementId: 'esa12', type: 'button', title: 'Далеше', nextSchemeId: 'scheme1', nextSchemeNodeId: 'sa2' }
+            ]
+         },
+         s2a: {
+            schemeNodeId: 'sa2',
+            schemeId: 'scheme1',
+            elements: [
+               { elementId: 'esa21', elementType: 'text', elementText: 'Запишите все, что сказал' },
+               { elementId: 'esa22', type: 'button', title: 'Далеше', nextSchemeId: 'scheme1', nextSchemeNodeId: 'sa3' }
+            ]
+         }
+      }
+   },
+   scheme2: {
+      schemeId: 'scheme2',
+      title: 'Поиcк работы',
+      schemeNodes: {
+         sb1: {
+            schemeNodeId: 'sb1',
+            schemeId: 'scheme1',
+            elements: [
+               { elementId: 'esb11', elementType: 'text', elementText: 'Вы выложили резюме на HH' },
+               {
+                  elementId: 'esb12',
+                  type: 'button',
+                  title: 'Вы сами откликнулись и Вас внесли в базу',
+                  nextSchemeId: 'scheme2',
+                  nextSchemeNodeId: 'sb2'
+               },
+               {
+                  elementId: 'esb13',
+                  type: 'button',
+                  title: 'Вы сыми откликнулиси и Вас пригласили на собеседование',
+                  nextSchemeId: 'scheme2',
+                  nextSchemeNodeId: 'sb3'
+               }
+            ]
+         },
+         sb2: {
+            schemeNodeId: 'sb2',
+            schemeId: 'scheme1',
+            elements: [{ elementId: '2esb21', elementType: 'text', elementText: 'Ну ждите теперь' }]
+         },
+         sb3: {
+            schemeNodeId: 'sb3',
+            schemeId: 'scheme2',
+            elements: [{ elementId: 'esb31', elementType: 'text', elementText: 'Ура!' }]
+         }
+      }
+   }
+}
