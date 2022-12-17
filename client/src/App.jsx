55d6701396f23396d307aaae18a3e3c4338dd766
OlgaXanadu <oevkarpova@gmail.com>
2022-12-17 21:00:46 +0300

    create layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import { AppRoutes } from './routes/Routes.jsx'
-import {Header} from '../src/components/Header';
+
 
 export const App = () => {
    return (
       <>
-         <Header />
          <AppRoutes />
       </>
    )
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from './Header'
+import { Outlet } from 'react-router-dom'
+
+export const Layout = () => {
+  return (<>
+    <Header/>
+    <Outlet />
+  </>)
+}
+
