f783250b052906f1a42308d36627828a5724269e
Cherry <choduraatyulyush@mail.ru>
2022-12-17 20:54:44 +0300

    header fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,91 +1,16 @@
-import { useState } from 'react';
-import { Navbar, Tooltip, UnstyledButton, createStyles, Stack } from '@mantine/core';
-import {
-  IconHome2,
-  IconArrowBarLeft,
-} from '@tabler/icons';
 import { useNavigate } from 'react-router-dom';
-
-const useStyles = createStyles((theme) => ({
-  link: {
-    width: 50,
-    height: 50,
-    borderRadius: theme.radius.md,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-    },
-  },
-
-  active: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-    },
-  },
-}));
-
-
-function NavbarLink({ icon: Icon, label, active, onClick }) {
-  const { classes, cx } = useStyles();
-  return (
-    <Tooltip label={label} position="right" transitionDuration={0}>
-      <UnstyledButton onClick={onClick} className={cx(classes.link, { [classes.active]: active })}>
-        <Icon stroke={1.5} />
-      </UnstyledButton>
-    </Tooltip>
-  );
-}
-
-
-
-
+import '../../styles.css';
 export function Header() {
-  const [active] = useState(2);
+  // const [value, setValue] = React.useState(0);
   const navigate = useNavigate();
 
-const home = [
-  { icon: IconHome2, label: 'Home'},
-];
-const back = [
-  { icon: IconArrowBarLeft, label: 'Back' },
-];
-
-
-  const homeView = home.map((link, index) => (
-    <NavbarLink
-      {...link}
-      key={link.label}
-      active={index === active}
-      onClick={() => navigate('/')}
-    />
-  ));
-  const backView = back.map((link, index) => (
-    <NavbarLink
-      {...link}
-      key={link.label}
-      active={index === active}
-      onClick={() => navigate(-1)}
-    />
-  ));
-
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
   return (
-    <Navbar height={750} width={{ base: 80 }} p="md">
-      <Navbar.Section grow mt={90}>
-        <Stack justify="center" spacing={800}>
-          {homeView}
-          </Stack>
-        </Navbar.Section>
-        <Navbar.Section grow mt={90}>
-        <Stack justify="center" spacing={600}>
-          {backView}
-        </Stack>
-      </Navbar.Section>
-      
-    </Navbar>
+   <header className='header'>
+    <button className='navButton' onClick={()=> navigate(-1)}>Назад</button>
+    <button className='navButton' onClick={()=> navigate('/')}>На главную</button>
+   </header>
   );
 }
