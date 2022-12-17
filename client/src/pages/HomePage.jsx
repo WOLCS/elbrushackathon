b3260f10821fc64681f13830323249b4e7072346
Cherry <choduraatyulyush@mail.ru>
2022-12-17 21:45:02 +0300

    styles_added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 import { useSelector } from 'react-redux'
 import { selectSchemes } from '../store/botReducer'
 import { Flex, Button, Title, Container, Center} from '@mantine/core';
+import '../../styles.css';
 
 export const HomePage = () => {
    const categories = useSelector(selectSchemes)
    return (
-      <> <Container><Center style={{ width: 400, height: 200 }}>
+      <> <Container className="homeTitle"><Center style={{ width: 500, height: 200 }}>
     <Title order={1}>HOME PAGE</Title>
     </Center>
          <Flex
       mih={150}
-      bg="rgba(0, 0, 0, .3)"
+      bg="rgb(255, 255, 255)"
       gap="md"
       justify="center"
       align="center"
@@ -18,7 +19,7 @@ export const HomePage = () => {
       wrap="wrap"
     >   
          {categories.map(category => (
-               <Button key={category} radius="md" size="xl" uppercase>{category}</Button>
+               <Button className="categoryButton"key={category} radius="md" size="xl" uppercase>{category}</Button>
          ))}
                
          </Flex>
