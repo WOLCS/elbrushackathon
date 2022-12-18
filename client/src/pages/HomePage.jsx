5e8a1e55dc28af3de8203443854a25bb58ec3fbe
Waldemar Kunz <w_kunz@web.de>
2022-12-18 12:28:45 +0100

    improve styles

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,8 @@
 import { Link } from 'react-router-dom'
 import { Flex, Button, Title, Container, Center } from '@mantine/core'
-
 import { LoadingOverlay } from '../components'
 import { ServerErrorPage } from './500'
 import { useGetSchemesQuery } from '../api/botApi'
-import '../../styles.css'
 
 export const HomePage = () => {
    const { data: categories, isLoading, isSuccess, isError } = useGetSchemesQuery()
@@ -16,20 +14,14 @@ export const HomePage = () => {
    ) : (
       <Container className="homeTitle">
          <Center>
-            <Title color={'#4520ab'} order={1}>ТEМА ОБУЧЕНИЯ</Title>
+            <Title color={'#4520ab'} order={1}>
+               ТEМА ОБУЧЕНИЯ
+            </Title>
          </Center>
-         <Flex
-            mih={300}
-            bg="#f3f1f8;"
-            gap="md"
-            justify="center"
-            align="center"
-            direction="column"
-            wrap="wrap"
-         >
+         <Flex mih={300} bg="#f3f1f8;" gap="md" justify="center" align="center" direction="column" wrap="wrap">
             {categories?.map(({ title, startSchemeNodeId }) => (
                <Link to={'/' + startSchemeNodeId} key={title}>
-                  <Button className="categoryButton" radius="md" size="xl" uppercase>
+                  <Button className="categoryButton" size="xl" uppercase>
                      {title}
                   </Button>
                </Link>
