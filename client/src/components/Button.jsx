ef9b023955af391e1c029e778240dcd6729f9218
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-12-18 12:14:17 +0100

    update db

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -4,24 +4,23 @@ import { Button as BaseButton } from '@mantine/core'
 const linkStyle = {
    display: 'flex',
    justifyContent: 'center',
-   textDecoration: "none"
+   textDecoration: 'none'
 }
 
 export const Button = ({ children, nextSchemeNodeId }) => (
-      <Link to={'/' + nextSchemeNodeId} style={linkStyle}>
-         <BaseButton
-            sx={theme => ({
-               padding: `${theme.spacing.sm}px ${theme.spacing.lg}px`,
-               '&:hover': {
-                  boxShadow: '0px 4px 20px rgb(255 188 91 / 70%)'
-               },
-               '&:active': {
-                  boxShadow: '0px 4px 20px rgb(255 188 91 / 70%)'
-               }
-            })}
-         >
-            {children}
-         </BaseButton>
-      </Link>
-   )
-
+   <Link to={'/' + nextSchemeNodeId} style={linkStyle}>
+      <BaseButton
+         sx={theme => ({
+            padding: `${theme.spacing.sm}px ${theme.spacing.lg}px`,
+            '&:hover': {
+               boxShadow: '0px 4px 20px rgb(34 139 230 / 70%)'
+            },
+            '&:active': {
+               boxShadow: '0px 4px 20px rgb(34 139 230 / 70%)'
+            }
+         })}
+      >
+         {children}
+      </BaseButton>
+   </Link>
+)
