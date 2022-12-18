fa44438c7bbb28ac5a8c8b3864cecee7c7d2a663
LAPTEVA Sophia 00061052 <sophia.lapteva@3ds.com>
2022-12-18 11:29:38 +0100

    create login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider as StoreProvider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { ApiProvider } from '@reduxjs/toolkit/dist/query/react'
-import { botApiSlice } from './api/botApi'
 import { MantineProvider } from '@mantine/core'
 import { store } from './store/store'
 import { App } from './App'
@@ -12,11 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <StoreProvider store={store}>
          <BrowserRouter>
-            <ApiProvider api={botApiSlice}>
-               <MantineProvider withGlobalStyles withNormalizeCSS>
-                  <App />
-               </MantineProvider>
-            </ApiProvider>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+               <App />
+            </MantineProvider>
          </BrowserRouter>
       </StoreProvider>
    </React.StrictMode>
---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Container, Flex, Header, ScrollArea, Table } from '@mantine/core'
+import { useListState } from '@mantine/hooks'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import '../../styles.css'
+import { useGetSchemesFullQuery } from '../api/botApi'
+export const AdminPage = () => {
+   console.log('sdfsdf')
+   const { data, isLoading, isError } = useGetSchemesFullQuery()
+   const [state, handlers] = useListState(data)
+   React.useEffect(() => {
+      handlers.setState(data)
+   }, [isLoading])
+   console.log(state, handlers)
+   const items = state?.map((item, index) => (
+      <Draggable key={item.symbol} index={index} draggableId={item.symbol}>
+         {(provided, i) => (
+            <tr ref={provided.innerRef} key={i} {...provided.draggableProps}>
+               <td>
+                  <div {...provided.dragHandleProps}>!!!</div>
+               </td>
+               <td style={{ width: 80 }}>{item.schemeId}</td>
+               <td style={{ width: 120 }}>{item.title}</td>
+               <td style={{ width: 120 }}>{item.startSchemeNodeId}</td>
+               <td style={{ width: 80 }}>{JSON.stringify(item.schemeNodes?.[0])}</td>
+               <td>{item.mass}</td>
+            </tr>
+         )}
+      </Draggable>
+   ))
+
+   return (
+      <Container size="sm">
+         <Flex gap="xl" align="stretch" direction="column">
+            <Header height={30}>Admin page</Header>
+            <ScrollArea>
+               <DragDropContext
+                  onDragEnd={({ destination, source }) =>
+                     handlers.reorder({ from: source.index, to: destination?.index || 0 })
+                  }
+               >
+                  <Table sx={{ minWidth: 420, '& tbody tr td': { borderBottom: 0 } }}>
+                     <thead>
+                        <tr>
+                           <th style={{ width: 40 }} />
+                           <th style={{ width: 80 }}>Position</th>
+                           <th style={{ width: 120 }}>Name</th>
+                           <th style={{ width: 40 }}>Symbol</th>
+                           <th>Mass</th>
+                        </tr>
+                     </thead>
+                     <Droppable droppableId="dnd-list" direction="vertical">
+                        {provided => (
+                           <tbody {...provided.droppableProps} ref={provided.innerRef}>
+                              {items}
+                              {provided.placeholder}
+                           </tbody>
+                        )}
+                     </Droppable>
+                  </Table>
+               </DragDropContext>
+            </ScrollArea>
+         </Flex>
+      </Container>
+   )
+}
